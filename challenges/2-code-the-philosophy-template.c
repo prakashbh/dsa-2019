abba98caff2ef88a9d528e0a1ca1758a76ed3b43
Prakash B Hegade <prakash.hegade@gmail.com>
2019-08-08 02:08:21 +0530

    Add files via upload

---
+++ challenges/2-code-the-philosophy-template.c
@@ -0,0 +1,143 @@
+/*****************************************************
+             CODE THE PHILOSOPHY CHALLENGE
+             Data Structures and Algorithms
+                    19ECSC201
+#+Name:
+#+Roll:
+#+SRN:
+#+Email:
+*****************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+Add a short comment about how you realized the philosophy using the program
+**/
+ void newtons_third_law()
+ {
+     printf("Challenge One: Newtons Third Law\n");
+     // TO-DO
+ }
+
+
+
+ /**
+Add a short comment about how you realized the philosophy using the program
+**/
+ void causal_theory()
+ {
+     printf("Challenge Two: Causal Theory\n");
+     // TO-DO
+ }
+
+
+
+/**
+Add a short comment about how you realized the philosophy using the program
+**/
+ void sufficient_reason()
+ {
+     printf("Challenge Three: Sufficient Reason\n");
+     // TO-DO
+ }
+
+
+
+ /**
+Add a short comment about how you realized the philosophy using the program
+**/
+ void the_mean()
+ {
+     printf("Challenge Four: The Mean\n");
+     // TO-DO
+ }
+
+
+
+/**
+Add a short comment about how you realized the philosophy using the program
+**/
+ void principle_of_evidence()
+ {
+     printf("Challenge Five: Principle of Evidence\n");
+     // TO-DO
+ }
+
+
+
+
+/**
+Add a short comment about how you realized the philosophy using the program
+**/
+ void free_lunch()
+ {
+     printf("Challenge Six: No Free Lunch\n");
+     // TO-DO
+ }
+
+
+
+
+ /**
+Add a short comment about how you realized the philosophy using the program
+**/
+ void occams_razor()
+ {
+     printf("Challenge Seven: Occam's Razor\n");
+     // TO-DO
+ }
+
+
+
+
+
+ /**
+Add a short comment about how you realized the philosophy using the program
+**/
+ void brooks_law()
+ {
+     printf("Challenge Eight: Brook's Law\n");
+     // TO-DO
+ }
+
+
+
+
+ /**
+Add a short comment about how you realized the philosophy using the program
+**/
+ void mark_twain()
+ {
+     printf("Challenge Nine: Mark Twain\n");
+     // TO-DO
+ }
+
+
+
+
+ /**
+Add a short comment about how you realized the philosophy using the program
+**/
+ void murphys_law()
+ {
+     printf("Challenge Ten: Murphy's Law\n");
+     // TO-DO
+ }
+
+int main()
+{
+    printf("The Challenge Begins\n");
+    newtons_third_law();
+    causal_theory();
+    sufficient_reason();
+    the_mean();
+    principle_of_evidence();
+    free_lunch();
+    occams_razor();
+    brooks_law();
+    mark_twain();
+    murphys_law();
+
+    return 0;
+}
