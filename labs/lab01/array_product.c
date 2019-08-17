7e499e718818a85157849078d5ff18ef813b49f4
Prakash B Hegade <prakash.hegade@gmail.com>
2019-08-18 03:09:46 +0530

    Add files via upload

---
+++ labs/lab01/array_product.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int array[5] = {2,3,4,6,8}
+    int product = 0;
+    int index = 0
+
+    for (index = 0;index < 10; index++)
+    {
+        product = product * i[array];
+    }
+
+    printf("Array product is %f\n" product);
+    return 0;
+}
